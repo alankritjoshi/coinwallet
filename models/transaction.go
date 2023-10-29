a4bc93fb18b026bfe1d2ea53887c5062cb97499b
Alankrit Joshi <alankritjoshi@gmail.com>
2023-10-28 17:01:30 -0700

    Simplify transaction model

--- models/transaction.go
+++ models/transaction.go
@@ -1,17 +1,12 @@
 package models
 
 type Transaction struct {
-	Id          string `db:"id" json:"id"`
-	Address     string `db:"address" json:"address"`
-	Hash        string `db:"hash" json:"hash"`
-	Size        int64  `db:"size" json:"size"`
-	Timestamp   string `db:"timestamp" json:"timestamp"`
-	Index       int64  `db:"index" json:"index"`
-	RelayedBy   string `db:"relayed_by" json:"relayed_by"`
-	BlockHeight int64  `db:"block_height" json:"block_height"`
-	InputsSize  int64  `db:"inputs_size" json:"inputs_size"`
-	OutputSize  int64  `db:"outputs_size" json:"outputs_size"`
-	Version     string `db:"version" json:"version"`
-	Inputs      string `db:"inputs" json:"inputs"`
-	Outputs     string `db:"outputs" json:"outputs"`
+	Id          string   `db:"id" json:"id"`
+	From        []string `db:"from" json:"from"`
+	To          []string `db:"to" json:"to"`
+	Hash        string   `db:"hash" json:"hash"`
+	Index       int64    `db:"index" json:"index"`
+	RelayedBy   string   `db:"relayed_by" json:"relayed_by"`
+	BlockHeight int64    `db:"block_height" json:"block_height"`
+	Version     string   `db:"version" json:"version"`
 }
