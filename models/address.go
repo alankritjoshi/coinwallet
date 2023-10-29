80f649c64c0004ab06059b26d6f5b39b51691fcc
Alankrit Joshi <alankritjoshi@gmail.com>
2023-10-28 17:01:15 -0700

    Add balance to address

--- models/address.go
+++ models/address.go
@@ -5,4 +5,5 @@ type Address struct {
 	WalletId          string `db:"wallet_id" json:"wallet_id"`
 	BlockchainAddress string `db:"blockchain_address" json:"blockchain_address"`
 	Label             string `db:"label" json:"label"`
+	Balance           int64  `db:"balance" json:"balance"`
 }
